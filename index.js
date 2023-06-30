c47e5dad76dbb99bd812208518436c1c83b58167
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2023-06-29 20:47:55 -0500

    not renderingBooks

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 function fetchBooks() {
-fetch('https://anapioficeandfire.com/api/books')
-.then((res)=> res.json())
-.then((data)=> console.log(data))
-  
+  fetch("https://anapioficeandfire.com/api/books")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 }
-
+renderBooks(fetchBooks);
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
+
+// function fetchBooks() {
+//   fetch("https://anapioficeandfire.com/api/books")
+//     .then((response) => {
+//       if (response.ok) {
+//         return response.json();
+//       } else {
+//         throw new Error("Error: " + response.status);
+//       }
+//     })
+//     .then((data) => {
+//       // Handle the response data
+//       console.log(data);
+//     })
+//     .catch((error) => {
+//       // Handle any errors that occurred during the request
+//       console.error(error);
+//     });
+// }
